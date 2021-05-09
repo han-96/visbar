18175a2bd3350cb95884e94704c058a679c72c7c
han-96 <hangvu.96@outlook.com.vn>
2021-05-09 18:03:01 +0700

    all4route Auth in authCtrl

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -23,7 +23,7 @@ const authCtrl = {
                 fullname, username: newUserName, email, password: passwordHash, gender
             });
             
-            const access_token = createAcessToken({id: newUser._id});
+            const access_token = createAccessToken({id: newUser._id});
             const refresh_token = createRefreshToken({id: newUser._id});
             
             res.cookie('refreshtoken', refresh_token, {
@@ -50,6 +50,32 @@ const authCtrl = {
     },
     login: async (req, res) => {
         try {
+            const { email, password } = req.body;
+
+            const user = await Users.findOne({email}).populate("followers following", "-password");
+
+            if (!user) return res.status(400).json({msg: "Địa chỉ email này chưa được đăng ký!"})
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) return res.status(400).json({msg: "Mật khẩu không chính xác!"})
+
+            const access_token = createAccessToken({id: user._id});
+            const refresh_token = createRefreshToken({id: user._id});
+            
+            res.cookie('refreshtoken', refresh_token, {
+                httpOnly: true,
+                path: '/api/refresh_token',
+                maxAge: 7*24*60*60*1000, //7 days
+            });
+
+            res.json({
+                msg: "Đăng nhập thành công! Welcome back!",
+                access_token,
+                user: {
+                    ...user._doc,
+                    password: '' 
+                }
+            })
 
         } catch (err) {
             return res.status(500).json({msg: err.message})
@@ -57,21 +83,39 @@ const authCtrl = {
     },
     logout: async (req, res) => {
         try {
-
+            res.clearCookie('refreshtoken', {path: './api.refresh_token'});
+            return res.json({msg: "Bạn vừa đăng xuất khỏi Visbar. Hãy quay lại sớm nhé!"})
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
     },
     generateAccessToken: async (req, res) => {
         try {
+            const rf_token = req.cookies.refreshtoken;
+            if(!rf_token) return res.status(400).json({msg: "Hãy đăng nhập!"});
+            
+            jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, async(err, result) => {
+                if (err) return res.status(400).json({msg: "Hãy đăng nhập!"})
+                
+                const user = await Users.findById(result.id).select('-password').populate('followers following', '-password');
 
+                if (!user) return res.status(400).json({msg: "Oop! Tài khoản này không tồn tại trong hệ thống"});
+
+                const access_token = createAccessToken({id: result.id});
+
+                res.json({
+                    access_token,
+                    user
+                });
+            })
+            
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
     }
 }
 
-const createAcessToken = (payload) => {
+const createAccessToken = (payload) => {
     return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'})
 };
 
