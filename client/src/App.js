a3dde65e5db29ac12f8c1f38adabf2dae1c83fef
han-96 <hangvu.96@outlook.com.vn>
2021-05-09 11:21:39 +0700

    Create some UI auth pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
-
 import './index.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import PageRender from './PageRender'
+
+
 
 function App() {
   return (
@@ -8,7 +10,8 @@ function App() {
       <input type="checkbox" id="theme"/>
       <div className="App">
         <div className="main">
-
+          <Route exact path="/:page" component={PageRender} />
+          <Route exact path="/:page/:id" component={PageRender} />
         </div>
       </div>
     </Router>
