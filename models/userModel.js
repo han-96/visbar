f89e20c5cb0fa69aee5ebb80145b6f010cff21ac
han-96 <hangvu.96@outlook.com.vn>
2021-05-09 15:53:46 +0700

    Create userSchema

---
+++ models/userModel.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 25
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        default: '../photos/noavatar.png'
+    },
+    role: {type: String, default: 'user'},
+    gender: {type: String, default: 'male'},
+    mobile: {type: String, default: ''},
+    address: {type: String, default: ''},
+    sologan: {
+        type: String,
+        default: '',
+        maxlength: 200
+    },
+    website: {type: String, default: ''},
+    followers: [
+        {
+            type: mongoose.Types.ObjectId, 
+            ref: 'user'
+        }
+    ],
+    following: [
+        {
+            type: mongoose.Types.ObjectId, 
+            ref: 'user'
+        }
+    ],
+}, {
+    timestamps: true
+})
+
+
+module.exports = mongoose.model('user', userSchema)
