d1cd532ba2884b065a5182870bb2d54313567f18
han-96 <hangvu.96@outlook.com.vn>
2021-05-10 00:58:08 +0700

    Login Almost Finish

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -8,6 +8,8 @@ const Login = () => {
     const initialState = { email: '', password: '' };
     const [userData, setUserData] = useState(initialState);
     const { email, password } = userData;
+
+    const [typePass, setTypePass] = useState(false) 
     
     const dispatch = useDispatch();
 
@@ -26,13 +28,18 @@ const Login = () => {
             <form onSubmit={handleSubmit}>
                 <h3 className="text-uppercase text-center mb-4" >Đăng nhập</h3>
                 <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email:</label>
                     <input type="email" className="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp" onChange={handleChangeInput} value={email}/>
                 </div>
 
                 <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" name="password" onChange={handleChangeInput} value={password}/>
+                    <label htmlFor="exampleInputPassword1" className="form-label">Mật khẩu:</label>
+                    <div className="pass">
+                        <input type={ typePass ? "text": "password"} className="form-control" id="exampleInputPassword1" name="password" onChange={handleChangeInput} value={password}/>
+                        <small onClick={() => setTypePass(!typePass)}>
+                            {typePass ? 'Ẩn mật khẩu' : 'Hiện mật khẩu'}
+                        </small>
+                    </div>
                 </div>
 
                 <button type="submit" className="btn btn-dark w-100" disabled={email && password ? false : true}>Đăng nhập</button>
--- client/src/redux/actions/authAction.js
+++ client/src/redux/actions/authAction.js
@@ -1,12 +1,28 @@
+import { postDataAPI } from '../../utils/fetchData'
+
 export const TYPES = {
     AUTH: 'AUTH'
 }
 
-export const login = (data) => (dispatch) => {
+export const login = (data) => async (dispatch) => {
     try {
         dispatch({type: 'NOTIFY', payload: {loading: true} })
-        
-    } catch (err) {
+        const res = await postDataAPI('login', data);
+        localStorage.setItem("firstLogin", true);
+        dispatch({
+            type: 'AUTH', 
+            payload: {
+                token: res.data.access_token,
+                user: res.data.user
+            }
+        })
 
+    } catch (err) {
+        dispatch({
+            type: 'NOTIFY', 
+            payload: {
+                error: err.response.data.msg
+            }
+        })
     }
 }
